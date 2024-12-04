b81ef6ffb8272eedeb947cb3a1e019cb8fc3eb43
Thiago <tsilva@techsocial.com.br>
2024-12-04 17:13:35 -0300

    Criação das Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ContatoController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ClienteController::class, 'index'])->name('home');
+Route::get('/clientes/search', [ClienteController::class, 'search'])->name('clientes.search');
+
+
+Route::resource('/clientes', ClienteController::class)->except(['show']);
+Route::resource('/contatos', ContatoController::class)->except(['show']);
