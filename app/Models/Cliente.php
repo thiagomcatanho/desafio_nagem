f5876cbe76d23b583f0522a44b05fa58d079572c
Thiago <tsilva@techsocial.com.br>
2024-12-04 17:15:56 -0300

    Criação das Models

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cliente extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         'nome',
@@ -16,8 +17,14 @@ class Cliente extends Model
         'endereco'
     ];
 
-    public function contatos(): HasMany
+    protected function cnpj(): Attribute
     {
-        return $this->hasMany(Contato::class, 'id_cliente');
+        return Attribute::make(
+            get: fn(string $value) => preg_replace(
+                '/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/',
+                '$1.$2.$3/$4-$5',
+                $value
+            ),
+        );
     }
 }
--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,15 +12,37 @@ class Contato extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'id_cliente',
+        'cliente_id',
         'nome_contato',
         'email_contato',
         'fone_contato',
         'cpf'
     ];
 
+    protected function cpf(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => preg_replace(
+                '/^(\d{3})(\d{3})(\d{3})(\d{2})$/',
+                '$1.$2.$3-$4',
+                $value
+            ),
+        );
+    }
+
+    protected function foneContato(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => preg_replace(
+                '/^(\d{2})(\d{5})(\d{4})$/',
+                '($1)$2-$3',
+                $value
+            ),
+        );
+    }
+
     public function cliente(): BelongsTo
     {
-        return $this->belongsTo(Cliente::class, 'id_cliente', 'id');
+        return $this->belongsTo(Cliente::class, 'cliente_id', 'id')->withTrashed();
     }
 }
